6216f32a42c0e113506c353e42b51c08fca60390
UltraV <1216564567@qq.com>
2024-05-11 15:33:37 +0800

    f

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+      <div class="hide"></div>
+      <iframe class="outer" id="frame" frameborder="0" :src="url"></iframe>
+  </div>
+  </template>
+   
+  <script setup>
+  const url = `https://www.coze.com/store/bot/7363119830733586450?panel=1&bid=6cfosji3c3008`
+  </script>
+   
+  <style scoped>
+  .container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin: 0 auto;
+      height: 90vh;
+      overflow: hidden;
+  }
+  .hide {
+      height: 12vh;
+      z-index: 100;
+      position: fixed;
+      top: 0;
+      left: 0;
+      right: 0;    
+      background-color: #fff;
+  }
+  .outer {
+      margin-top: 20px;
+      width: 1000px;
+      height: 100%;
+  }
+  </style>
+   
